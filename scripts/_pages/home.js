d00368b1e4a40bcd702eaf45bbe6fd00b1b53cab
Drinal <drinalmail@gmail.com>
2014-05-30 00:00:05 -0500

    .

--- scripts/_pages/home.js
+++
@@ -1,6 +0,0 @@
-/* start /scripts/_pages/home.js*/
-
-var a=Math.floor(Math.random()*10)+1;if(a==8){var preloader=(new Image()).src="/images/prelaunch/fred.png"
-$('body').append('<audio id="fred_audio" preload="auto"><source src="/includes/audio/SFX_FACE_SCREAM_01.mp3" type="audio/mp3"></audio>');setTimeout(function(){$('body').append("<div id='fred' style='width: 100%; height:100%; position: absolute; top: 0; background: url(/images/prelaunch/fred.png) no-repeat top center transparent; background-size: 100%;'></div>");document.getElementById('fred_audio').play();setTimeout(function(){$('#fred').fadeOut(50,function(){$('#fred').remove()})},300)},5000);};
-
-/* end /scripts/_pages/home.js*/
--- scripts/core.js
+++
@@ -1,5 +0,0 @@
-/* start /scripts/core.js*/
-
-window.h1z1={};
-
-/* end /scripts/core.js*/
